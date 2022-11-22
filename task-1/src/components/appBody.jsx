762047be809a3ff8c8dfe566bd9a6f694ae00657
Omer Kaplan <Omer.kaplan1998@cs.colman.ac.il>
2022-11-23 01:40:55 +0200

    witihout the bonus

--- task-1/src/components/appBody.jsx
+++ task-1/src/components/appBody.jsx
@@ -1,20 +1,18 @@
 import "../css/appBody.css";
-import {products} from "../data/products";
+import { products } from "../data/products";
 import Card from "./card";
 
 function AppBody() {
-  console.log(products);
   return (
     <div id="body-container">
       <div id="list-container">
-        {products.map((product) => (
-          <div>
-            <Card
-              title={product.title}
-              price={product.price}
-              imgUrl={product.firstPhotoUrl}
-            />
-          </div>
+        {products.map((product, index) => (
+          <Card
+            key={index}
+            title={product.title}
+            price={product.price}
+            imgUrl={product.firstPhotoUrl}
+          />
         ))}
       </div>
     </div>
--- task-1/src/components/card.jsx
+++ task-1/src/components/card.jsx
@@ -1,17 +1,20 @@
 import "../css/card.css";
 
 const onCardClick = () => {
-  alert("click");
+  alert("lol");
+  window.open("http://localhost:3000/product", "_blank", "noopener,noreferrer");
 };
 
 function Card(props) {
+  const { title, price, secondPhotoUrl, seller, description } = props;
+
   return (
     <div id="card-container" onClick={onCardClick}>
       <div id="product-title-price-container">
-        <span>{props.title}</span>
-        <span>{props.price}</span>
+        <div>{props.title}</div>
+        <div>{props.price}</div>
       </div>
-      <span>{props.imgUrl}</span>
+      <span>{props.imgUrl}Pic container</span>
     </div>
   );
 }
